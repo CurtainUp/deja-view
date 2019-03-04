2dfe644a956864721af4126d440b0b98c2b8308f
Elyse Dawson <dawson.elyse@gmail.com>
2019-03-04 15:45:27 -0600

    Connected css

--- deja/views.py
+++ deja/views.py
@@ -8,7 +8,7 @@ from deja.models import *
 
 # Create your views here.
 def index(request):
-    index_template = 'index.html'
+    index_template = "index.html"
     return render(request, index_template)
 
 def register_user(request):
@@ -18,12 +18,12 @@ def register_user(request):
     # Set to False initially. Code changes value to True when registration succeeds.
     registered = False
 
-    # Create a new user by invoking the `create_user` helper method on Django's built-in User model
-    if request.method == 'POST':
+    # Create a new user by invoking the `create_user` helper method on Django"s built-in User model
+    if request.method == "POST":
         user_form = UserForm(data=request.POST)
 
         if user_form.is_valid():
-            # Save the user's form data to the database.
+            # Save the user"s form data to the database.
             user = user_form.save()
 
             # Now we has the password with the set_password helper method.
@@ -36,40 +36,40 @@ def register_user(request):
 
         return login_user(request)
 
-    elif request.method == 'GET':
+    elif request.method == "GET":
         user_form = UserForm()
-        register_template = 'register.html'
-        return render(request, register_template, {'user_form': user_form})
+        register_template = "register.html"
+        return render(request, register_template, {"user_form": user_form})
 
 def login_user(request):
     '''Handles the login of a registered user for authentication'''
 
-    # Obtain the context for the user's request.
-    context = {'next': request.GET.get('next', '/')}
+    # Obtain the context for the user"s request.
+    context = {"next": request.GET.get("next", "/")}
 
     # If the request is a HTTP POST, try to pull out the relevant information.
-    if request.method == 'POST':
+    if request.method == "POST":
         print(request.POST)
 
         # Use the built-in authenticate method to verify
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST["username"]
+        password = request.POST["password"]
         authenticated_user = authenticate(username=username, password=password)
 
         # If authentication was successful, log the user in
         if authenticated_user is not None:
             login(request=request, user=authenticated_user)
-            if request.POST.get('next') == '/':
-                return HttpResponseRedirect('/')
+            if request.POST.get("next") == "/":
+                return HttpResponseRedirect("/")
             else:
-                print("ELSE STATEMENT:", request.POST.get('next', '/'))
-                return HttpResponseRedirect(request.POST.get('next', '/'))
+                print("ELSE STATEMENT:", request.POST.get("next", "/"))
+                return HttpResponseRedirect(request.POST.get("next", "/"))
 
         else:
             # Bad login details were provided. User cannot be logged in
             print("Invalid login details: {}, {}".format(username, password))
 
-    return render(request, 'login.html', context)
+    return render(request, "login.html", context)
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
@@ -78,4 +78,4 @@ def logout_user(request):
     logout(request)
 
     # Take the user back to the homepage.
-    return HttpResponseRedirect('/')
+    return HttpResponseRedirect("/")
