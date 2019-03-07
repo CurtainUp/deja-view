d25a0571faffc7d463addc264825427e87b2232c
Elyse Dawson <dawson.elyse@gmail.com>
2019-03-07 00:03:50 -0600

    Deja results displays uploaded image as well as match results

--- deja/api/sightengine.py
+++ deja/api/sightengine.py
@@ -2,14 +2,12 @@ from sightengine.client import SightengineClient
 import requests, json, os
 
 def get_celebs(url):
-
     client = SightengineClient('1460406796','huUawvG2vTAfE8aqCFvp')
     output = client.check('celebrities').set_url({url})
-
-    print("Output:" output)
-
     # Returns names and probabilities
     results = output["faces"][0]["celebrity"]
 
-    for celeb in results:
-        print("Name:", celeb["name"], "Probability:", celeb["prob"])
+    return results
+
+    # for celeb in results:
+    #     return ("Name:", celeb["name"], "Probability:", celeb["prob"])
