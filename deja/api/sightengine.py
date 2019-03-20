f9f39ff60ebe0580972465c2ca5000b8922c266e
Elyse Dawson <dawson.elyse@gmail.com>
2019-03-20 15:24:05 -0500

    Hid sightengine key

--- deja/api/sightengine.py
+++ deja/api/sightengine.py
@@ -1,8 +1,9 @@
 from sightengine.client import SightengineClient
 import requests, json, os
+import deja.local_settings as local_settings
 
 def get_celebs(url):
-    client = SightengineClient('1460406796','huUawvG2vTAfE8aqCFvp')
+    client = SightengineClient(local_settings.SIGHTENGINE_USER, local_settings.SIGHTENGINE_KEY)
     output = client.check('celebrities').set_url({url})
     # Returns names and probabilities
     results = output["faces"][0]["celebrity"]
