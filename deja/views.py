923830001c3ce9e3da7ce1c8ed656a42183488c6
Elyse Dawson <dawson.elyse@gmail.com>
2019-03-20 11:15:09 -0500

    Styled note form to be consistent, adjusted watchlist style

--- deja/views.py
+++ deja/views.py
@@ -12,6 +12,8 @@ from deja.api.sightengine import get_celebs
 import imdb
 from imdb.Person import Person
 
+# -----Firebase key and integration of pyrebase----- #
+
 config = {
     "apiKey": "AIzaSyAAw5FV9aw2OJBCMwLV8TVIJemk0dTQR38",
     "authDomain": "dejaview-d81e4.firebaseapp.com",
@@ -23,8 +25,12 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 
-# Create your views here.
+# ---------- #
+
+# -----VIEWS----- #
+
 def index(request):
+    '''View for landing page'''
     return render(request, "index.html")
 
 def register_user(request):
@@ -39,19 +45,20 @@ def register_user(request):
         user_form = UserForm(data=request.POST)
 
         if user_form.is_valid():
-            # Save the user"s form data to the database.
+            # Save the user's form data to the database.
             user = user_form.save()
 
-            # Now we has the password with the set_password helper method.
-            # Once hashed, we can update the user object.
+            # Collect password with the set_password helper method.
             user.set_password(user.password)
+            # Once hashed, update the user object.
             user.save()
 
-            # Update our variable to tell the template registration was successful.
+            # Update variable to tell the template registration was successful.
             registered = True
 
         return login_user(request)
 
+    # Load registration form
     elif request.method == "GET":
         user_form = UserForm()
         register_template = "register.html"
@@ -60,7 +67,7 @@ def register_user(request):
 def login_user(request):
     '''Handles the login of a registered user for authentication'''
 
-    # Obtain the context for the user"s request.
+    # Obtain the context for the user's request.
     context = {"next": request.GET.get("next", "/")}
 
     # If the request is a HTTP POST, try to pull out the relevant information.
@@ -86,10 +93,8 @@ def login_user(request):
 
     return render(request, "login.html", context)
 
-# Use the login_required() decorator to ensure only those logged in can access the view.
 @login_required
 def logout_user(request):
-    # Since we know the user is logged in, we can now just log them out.
     logout(request)
 
     # Take the user back to the homepage.
@@ -100,9 +105,9 @@ def deja(request):
     ''' View for user to create a Deja '''
 
     # Creates an instance of a Deja in database upon clicking "Deja"
-    current_user = request.user
-
     if request.method == 'POST':
+        current_user = request.user
+
         img_url = request.POST["url"]
         user = User.objects.get(pk=current_user.id)
 
@@ -117,18 +122,22 @@ def deja(request):
 def deja_results(request, deja_id):
     ''' View for user to view Actor matches and view that performer's most recent projects '''
 
-    # Stores the url of the most recently uploaded image
-    uploaded_img = Deja.objects.get(pk=deja_id).img_url
-    # Submits url to Sight Engine API
-    results = get_celebs(uploaded_img)
-    print(results[0])
+    # ----- FACIAL RECOGNITION -----
 
-    # Saves top match to Results
+    # Stores the Firebase url of the most recently uploaded image
+    uploaded_img = Deja.objects.get(pk=deja_id).img_url
+    # Submits url to Sight Engine API and returns top five possible matches
+    results = get_celebs(uploaded_img)
+
+    # ----------
+
+    # Saves top match to Results table
     if results:
         try:
+            # Checks if result has already been saved
             top_match = Result.objects.get(pk=deja_id)
-            print("result already logged: ", top_match)
         except:
+            # If no instance, create top match entry in Results
             deja = Deja.objects.get(pk=deja_id)
             name = results[0]['name']
             probability = results[0]['prob']
@@ -136,13 +145,16 @@ def deja_results(request, deja_id):
             new_result = Result(deja=deja, name=name, probability=probability)
             new_result.save()
 
-
-
+    # Handles all possible posts on deja_results page
     if request.method == 'POST':
 
+        # If user clicks on a celebrity match, they will be presented with the celeb and their 5 most recent credits
         if request.POST.get('celeb_name'):
             celeb_name = request.POST['celeb_name']
 
+            # Most recent filmography load is saved in session storage for faster recall
+
+            # Checks if a filmography load for the clicked celeb exists in session storage
             if request.session.get('credits') and request.session['credits']['celeb_name'] == celeb_name:
                 return render(request, "films.html", request.session['credits'])
             else:
@@ -153,9 +165,10 @@ def deja_results(request, deja_id):
                 person = ia.get_person(celebID)
                 filmography = person['filmography']
 
-                # Reduce list to most recent 5 entries to pass to template
+                # List of  5 most recent entries to pass to template
                 most_recent = []
 
+                # Loop through films and run logic before adding them to most_recent
                 for value in filmography[0].values():
                     for film in value:
                         if len(most_recent) < 5:
@@ -163,23 +176,33 @@ def deja_results(request, deja_id):
                             movie = ia.get_movie(movieID)
                             film_title = film['title']
 
+                            # Only grabs movies with a poster image to help filter out upcoming projects
                             if movie.get('cover url'):
                                 # Trims () that appears at the ends of upcoming projects
                                 if film_title.endswith("()"):
                                     film_title = film_title[:-3]
+
+                                # Create unique URL to add to film titles on filmography and watchlist pages
                                 link = "http://www.imdb.com/title/tt" + movieID + "/"
+
+                                # Dictionary of credit info
                                 credit = {'link': link, 'title': film_title}
+
+                                # Dictionary is added to most_recent list
                                 most_recent.append(credit)
 
-                # Returns url of actor headshot OR default message if none available!
+                # Returns URL of actor headshot OR default message if none available
                 no_headshot = "No headshot available"
                 headshot = person.get('full-size headshot', no_headshot)
 
                 context = {'headshot': headshot, 'most_recent': most_recent, 'celeb_name': celeb_name, 'celebID': celebID}
+
+                # Saves context to session storage for faster load if match is clicked consecutively. NOTE: Clicking on a different match replaces the session storage context.
                 request.session['credits'] = context
 
                 return render(request, "films.html", request.session['credits'])
 
+        # If user clicks pen button, loads note page
         elif request.POST.get("note"):
             return HttpResponseRedirect(reverse("deja:note", args=(deja_id,)))
 
